939b2008957f4eb6dbef6b1aa5cbfd621e979e02
Michael Neale <michael.neale@gmail.com>
2025-01-28 10:11:56 +1100

    hotfix: add a link to download stable for extensions site (#823)

--- extensions-site/app/components/header.tsx
+++ extensions-site/app/components/header.tsx
@@ -8,6 +8,8 @@ export const Header = () => {
   const location = useLocation();
   const { hash, pathname, search } = location;
 
+  const stableDownload = "https://github.com/block/goose/releases/download/stable/Goose.zip";
+
   return (
     <div className="bg-bgApp container mx-auto border-borderSubtle py-16">
       <div className="h-full flex justify-between items-center">
@@ -16,12 +18,10 @@ export const Header = () => {
         </NavLink>
         <div className="w-auto items-center flex">
           <Button>
-            <IconDownload />
-            {/* {pathname === "/" ? ( */}
-            <span className="ml-2">Download Goose for desktop</span>
-            {/* ) : (
-              <></>
-            )} */}
+            <a href={stableDownload}>
+              <IconDownload /></a>
+              <span className="ml-2"><a href={stableDownload}>Download Goose for desktop</a></span>
+            
           </Button>
           <ThemeToggle className="ml-4" />
         </div>
