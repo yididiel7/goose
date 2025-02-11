b19ede4b53445aaa057fffe5f41f82d412cd8dff
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2025-02-11 02:23:25 -0500

    fix: Add page title to extensions site (#950)

--- extensions-site/app/root.tsx
+++ extensions-site/app/root.tsx
@@ -31,6 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Extensions | codename goose</title>
         <Meta />
         <Links />
       </head>
