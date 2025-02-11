48c93213f4ad760f54941e9de1f218e293a24572
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2025-02-11 10:27:52 -0500

    fix: Add goose favicon to extensions site (#955)

--- extensions-site/app/root.tsx
+++ extensions-site/app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import stylesheet from "./styles/main.css?url";
 import { Header } from "./components/header";
+import favicon from "./assets/favicon.ico?url";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -23,6 +24,7 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
   { rel: "stylesheet", href: stylesheet },
+  { rel: "icon", type: "image/x-icon", href: favicon },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
