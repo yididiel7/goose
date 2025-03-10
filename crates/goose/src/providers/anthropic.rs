3b36591cb55e291d9de46ec98df025c317f0d13e
Salman Mohammed <smohammed@squareup.com>
2025-03-10 12:40:29 -0400

    fix: update anthropic provider headers (#1592)

--- crates/goose/src/providers/anthropic.rs
+++ crates/goose/src/providers/anthropic.rs
@@ -163,14 +163,18 @@ impl Provider for AnthropicProvider {
 
         let is_thinking_enabled = std::env::var("ANTHROPIC_THINKING_ENABLED").is_ok();
         if self.model.model_name.starts_with("claude-3-7-sonnet-") && is_thinking_enabled {
+            // https://docs.anthropic.com/en/docs/build-with-claude/extended-thinking#extended-output-capabilities-beta
+            headers.insert("anthropic-beta", "output-128k-2025-02-19".parse().unwrap());
+        }
+
+        if self.model.model_name.starts_with("claude-3-7-sonnet-") {
             // https://docs.anthropic.com/en/docs/build-with-claude/tool-use/token-efficient-tool-use
             headers.insert(
                 "anthropic-beta",
                 "token-efficient-tools-2025-02-19".parse().unwrap(),
             );
-            // https://docs.anthropic.com/en/docs/build-with-claude/extended-thinking#extended-output-capabilities-beta
-            headers.insert("anthropic-beta", "output-128k-2025-02-19".parse().unwrap());
         }
+
         // Make request
         let response = self.post(headers, payload.clone()).await?;
 
