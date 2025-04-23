fa8059231d4f7de8b6d7be0c7d548340e2db4408
zengqin huang <112330973+cuso4huang@users.noreply.github.com>
2025-04-23 23:57:14 +0800

    Add new model to  google.rs (#2311)

--- crates/goose/src/providers/google.rs
+++ crates/goose/src/providers/google.rs
@@ -22,6 +22,7 @@ pub const GOOGLE_KNOWN_MODELS: &[&str] = &[
     "gemini-2.0-flash-thinking-exp-01-21",
     "gemini-2.0-pro-exp-02-05",
     "gemini-2.5-pro-exp-03-25",
+    "gemini-2.5-flash-preview-04-17",
 ];
 
 pub const GOOGLE_DOC_URL: &str = "https://ai.google/get-started/our-models/";
